fbcf7d35626ae112d3e5b0a2f0ff649458f4dc07
jzenzinger <juris.zen@seznam.cz>
2021-02-12 17:09:44 +0100

    Initial Commit

---
+++ ViewsModifiers/ViewsModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsModifiersApp.swift
+//  ViewsModifiers
+//
+//  Created by Jiří Zenzinger on 12.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct ViewsModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
