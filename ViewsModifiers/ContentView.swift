224f72e6d7aa46291336202f59541768df6d6745
jzenzinger <juris.zen@seznam.cz>
2021-02-12 20:18:52 +0100

    Adding watermark extension to View

--- ViewsModifiers/ContentView.swift
+++ ViewsModifiers/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var usedText = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Color.blue
+            .frame(width: 300, height: 300)
+            .watermarked(with: "You're fucked up")
+    }
+}
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.title)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.gray)
+                .cornerRadius(3.0)
+                .opacity(0.8)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+            ContentView()
+        
     }
 }
